c4c05148dbb41d6f7b912fe4cf432649d0358eca
Sikarii <Sikarii@users.noreply.github.com>
2022-04-19 00:04:56 +0000

    Deploying to public from @ KZGlobalTeam/docs@389995f4e2c7091dedf656e5e5bd2402062c56d4 🚀

---
+++ assets/index.md.9456ecd1.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as i,o as a,a as t}from"./app.3bba0006.js";const m='{"title":"Home","description":"Home page","frontmatter":{"title":"Home","description":"Home page"},"headers":[],"relativePath":"index.md","lastUpdated":1650326640000}',l={},s=t("",6),d=[s];function n(r,o,g,u,h,c){return a(),i("div",null,d)}var f=e(l,[["render",n]]);export{m as __pageData,f as default};
